6468837c5c552863920d98125a31a86381b74a63
Sim-GyuBin <skb0606@gmail.com>
2023-11-15 21:53:42 +0900

    refactor: 할인 종류 열거형으로 관리

---
+++ src/main/java/christmas/constants/DiscountType.java
@@ -0,0 +1,46 @@
+package christmas.constants;
+
+public enum DiscountType {
+    CHRISTMAS("크리스마스 디데이 할인", -1000, 100),
+    WEEKDAY_DESSERT("평일 디저트 할인", -2023),
+    WEEKEND_MAIN_DISH("주말 메인 요리 할인", -2023),
+    SPECIAL("특별 할인", -1000),
+    GIFT_EVENT("증정 이벤트");
+
+    private final String name;
+    private final int discountAmount;
+    private final int increment;
+
+    DiscountType(String name, int discountAmount, int increment) {
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.increment = increment;
+    }
+
+    DiscountType(String name, int discountAmount) {
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.increment = 0;
+    }
+
+    DiscountType(String name) {
+        this.name = name;
+        this.discountAmount = 0;
+        this.increment = 0;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDiscountAmount() {
+        return discountAmount;
+    }
+    public int getIncrement() {
+        return increment;
+    }
+}
+
+
--- src/main/java/christmas/domain/DiscountCalculator.java
+++ src/main/java/christmas/domain/DiscountCalculator.java
@@ -1,16 +1,13 @@
 package christmas.domain;
 
+import christmas.constants.DiscountType;
+
 import java.time.LocalDate;
 import java.time.DayOfWeek;
 import java.util.Arrays;
 import java.util.List;
 
 public class DiscountCalculator {
-    private static final int CHRISTMAS_DISCOUNT_START = -1000;
-    private static final int DAILY_INCREMENT = -100;
-    private static final int WEEKDAY_DESSERT_DISCOUNT = -2023;
-    private static final int WEEKEND_MAIN_DISH_DISCOUNT = -2023;
-    private static final int SPECIAL_DISCOUNT = -1000;
     private final List<Integer> specialDiscountDays = Arrays.asList(3, 10, 17, 24, 25, 31);
     private static final Menu EVENT_GIFT = Menu.CHAMPAGNE;
     private static final int EVENT_GIFT_QUANTITY = 1;
@@ -27,36 +24,41 @@ public class DiscountCalculator {
     }
 
     private void addGiftEvent(DiscountDetails discountDetails, Order order) {
-        if(order.calculateTotalAmount() > GIFT_THRESHOLD_AMOUNT) {
+        if (order.calculateTotalAmount() > GIFT_THRESHOLD_AMOUNT) {
             order.addGift(EVENT_GIFT, EVENT_GIFT_QUANTITY);
-            discountDetails.addDiscount("증정 이벤트", EVENT_GIFT.getPrice() * -1);
+            discountDetails.addDiscount(DiscountType.GIFT_EVENT.getName(), EVENT_GIFT.getPrice() * -1);
         }
     }
 
     private void addChristmasDiscount(DiscountDetails discountDetails, LocalDate date) {
         if (date.getMonthValue() == 12 && date.getDayOfMonth() <= 25) {
-            int christmasDiscount = CHRISTMAS_DISCOUNT_START + (date.getDayOfMonth() - 1) * DAILY_INCREMENT;
-            discountDetails.addDiscount("크리스마스 디데이 할인", christmasDiscount);
+            String discountName = DiscountType.CHRISTMAS.getName();
+            int christmasDiscount = DiscountType.CHRISTMAS.getDiscountAmount() + (date.getDayOfMonth() - 1) * DiscountType.CHRISTMAS.getIncrement();
+            discountDetails.addDiscount(discountName, christmasDiscount);
         }
     }
 
     private void addWeekdayDessertDiscount(DiscountDetails discountDetails, Order order, LocalDate date) {
         if (isWeekday(date) && order.containsCategory(Menu.Category.DESSERT)) {
-            int dessertDiscount = order.countItemsByCategory(Menu.Category.DESSERT) * WEEKDAY_DESSERT_DISCOUNT;
-            discountDetails.addDiscount("평일 디저트 할인", dessertDiscount);
+            String discountName = DiscountType.WEEKDAY_DESSERT.getName();
+            int dessertDiscount = order.countItemsByCategory(Menu.Category.DESSERT) * DiscountType.WEEKDAY_DESSERT.getDiscountAmount();
+            discountDetails.addDiscount(discountName, dessertDiscount);
         }
     }
 
     private void addWeekendMainDishDiscount(DiscountDetails discountDetails, Order order, LocalDate date) {
         if (isWeekend(date) && order.containsCategory(Menu.Category.MAIN)) {
-            int mainDishDiscount = order.countItemsByCategory(Menu.Category.MAIN) * WEEKEND_MAIN_DISH_DISCOUNT;
-            discountDetails.addDiscount("주말 메인 요리 할인", mainDishDiscount);
+            String discountName = DiscountType.WEEKEND_MAIN_DISH.getName();
+            int mainDishDiscount = order.countItemsByCategory(Menu.Category.MAIN) * DiscountType.WEEKEND_MAIN_DISH.getDiscountAmount();
+            discountDetails.addDiscount(discountName, mainDishDiscount);
         }
     }
 
     private void addSpecialDiscount(DiscountDetails discountDetails, LocalDate date) {
         if (specialDiscountDays.contains(date.getDayOfMonth())) {
-            discountDetails.addDiscount("특별 할인", SPECIAL_DISCOUNT);
+            String discountName = DiscountType.SPECIAL.getName();
+            int mainSpecialDiscount = DiscountType.SPECIAL.getDiscountAmount();
+            discountDetails.addDiscount(discountName, mainSpecialDiscount);
         }
     }
 
