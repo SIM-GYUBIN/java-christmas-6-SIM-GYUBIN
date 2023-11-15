4414afd0881d457ac526f59f169dbbff052f0ebf
Sim-GyuBin <skb0606@gmail.com>
2023-11-15 21:04:04 +0900

    refactor: 단위 문자 상수처리

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -11,13 +11,16 @@ import java.util.Locale;
 public class OutputView {
     private final String GREETING_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private final String EVENT_PREVIEW_FORMAT = "%s에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    private final String ORDER_MENU_HEADER = "<주문 메뉴>";
-    private final String TOTAL_COST_BEFORE_DISCOUNT_HEADER = "<할인 전 총주문 금액>";
-    private final String GIFT_MENU_HEADER = "<증정 메뉴>";
-    private final String BENEFITS_DETAILS_HEADER = "<혜택 내역>";
-    private final String TOTAL_BENEFITS_AMOUNT_HEADER = "<총혜택 금액>";
-    private final String EXPECTED_PAYMENT_AFTER_DISCOUNT_HEADER = "<할인 후 예상 결제 금액>";
-    private final String DECEMBER_EVENT_BADGE_HEADER = "<12월 이벤트 배지>";
+    private final String ORDER_MENU_HEADER = "\n<주문 메뉴>";
+    private final String TOTAL_COST_BEFORE_DISCOUNT_HEADER = "\n<할인 전 총주문 금액>";
+    private final String GIFT_MENU_HEADER = "\n<증정 메뉴>";
+    private final String BENEFITS_DETAILS_HEADER = "\n<혜택 내역>";
+    private final String TOTAL_BENEFITS_AMOUNT_HEADER = "\n<총혜택 금액>";
+    private final String EXPECTED_PAYMENT_AFTER_DISCOUNT_HEADER = "\n<할인 후 예상 결제 금액>";
+    private final String DECEMBER_EVENT_BADGE_HEADER = "\n<12월 이벤트 배지>";
+    private final String UNIT_QUANTITY = "개";
+    private final String UNIT_CURRENCY = "원";
+    private final String TEXT_NONE = "없음";
 
     public void printError(String errorMessage) {
         System.out.println(errorMessage);
@@ -47,6 +50,7 @@ public class OutputView {
         System.out.println();
         printBadge(badge);
     }
+
     public void printMenu(Order order) {
         System.out.println(ORDER_MENU_HEADER);
 
@@ -57,19 +61,19 @@ public class OutputView {
 
     private void printCostBeforeDiscount(Order order) {
         System.out.println(TOTAL_COST_BEFORE_DISCOUNT_HEADER);
-        System.out.println(formatAmount(order.calculateTotalAmount()) + "원");
+        System.out.println(formatAmount(order.calculateTotalAmount()) + UNIT_CURRENCY);
     }
 
     private void printGift(Order order) {
         System.out.println(GIFT_MENU_HEADER);
 
         if (order.getGifts().isEmpty()) {
-            System.out.println("없음");
+            System.out.println(TEXT_NONE);
             return;
         }
 
         order.getGifts().forEach((menu, quantity) -> {
-            System.out.println(menu.getName() + " " + quantity + "개");
+            System.out.println(menu.getName() + " " + quantity + UNIT_QUANTITY);
         });
     }
 
@@ -77,30 +81,30 @@ public class OutputView {
         System.out.println(BENEFITS_DETAILS_HEADER);
 
         if (discountDetails.getDiscountDetails().isEmpty()) {
-            System.out.println("없음");
+            System.out.println(TEXT_NONE);
             return;
         }
 
         discountDetails.getDiscountDetails().forEach((discountType, amount) -> {
-            System.out.println(discountType + ": " + formatAmount(amount) + "원");
+            System.out.println(discountType + ": " + formatAmount(amount) + UNIT_CURRENCY);
         });
     }
 
     private void printTotalDiscounts(DiscountDetails discountDetails) {
         System.out.println(TOTAL_BENEFITS_AMOUNT_HEADER);
-        System.out.println(formatAmount(discountDetails.getTotalDiscount()) + "원");
+        System.out.println(formatAmount(discountDetails.getTotalDiscount()) + UNIT_CURRENCY);
     }
 
     private void printFinalAmount(int finalAmount) {
         System.out.println(EXPECTED_PAYMENT_AFTER_DISCOUNT_HEADER);
-        System.out.println(formatAmount(finalAmount) + "원");
+        System.out.println(formatAmount(finalAmount) + UNIT_CURRENCY);
     }
 
     private void printBadge(String badge) {
         System.out.println(DECEMBER_EVENT_BADGE_HEADER);
 
         if (badge.isEmpty()) {
-            System.out.println("없음");
+            System.out.println(TEXT_NONE);
             return;
         }
         System.out.println(badge);
