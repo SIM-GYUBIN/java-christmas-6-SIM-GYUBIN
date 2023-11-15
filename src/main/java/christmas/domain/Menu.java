1cb28037e569f5c6f586fc5cfb9869c29017286e
Sim-GyuBin <skb0606@gmail.com>
2023-11-15 22:00:46 +0900

    refactor: 주석제거

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -6,21 +6,17 @@ public enum Menu {
     MUSHROOM_SOUP("양송이수프", 6000, Category.APPETIZER),
     TAPAS("타파스", 5500, Category.APPETIZER),
     CAESAR_SALAD("시저샐러드", 8000, Category.APPETIZER),
-    // ... 나머지 애피타이저 항목들
 
     T_BONE_STEAK("티본스테이크", 55000, Category.MAIN),
     BBQ_RIBS("바비큐립", 54000, Category.MAIN),
     SEAFOOD_PASTA("해산물파스타", 35000, Category.MAIN),
-    // ... 나머지 메인 항목들
 
     CHOCOLATE_CAKE("초코케이크", 15000, Category.DESSERT),
     ICE_CREAM("아이스크림", 5000, Category.DESSERT),
-    // ... 나머지 디저트 항목들
 
     ZERO_COLA("제로콜라", 3000, Category.BEVERAGE),
     RED_WINE("레드와인", 60000, Category.BEVERAGE),
     CHAMPAGNE("샴페인", 25000, Category.BEVERAGE);
-    // ... 나머지 음료 항목들
 
     private final String name;
     private final int price;
