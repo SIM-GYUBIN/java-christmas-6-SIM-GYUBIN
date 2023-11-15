efb133985e0f17db2def37ce864144a48d0be1e2
Sim-GyuBin <skb0606@gmail.com>
2023-11-15 18:49:40 +0900

    feat: 주문할 메뉴와 수량을 입력

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -23,11 +23,43 @@ public class EventController {
         outputView.printGreeting();
         try {
             LocalDate date = inputView.readDate();
+            Order order = takeOrder();
         } catch (IllegalArgumentException e) {
             outputView.printError(e.getMessage());
             run(); // 재귀 호출로 에러 발생 시 다시 시작
         }
     }
 
+    private Order takeOrder() {
+        String orderedMenu = inputView.readMenu();
+        Order order = new Order();
+
+        // 입력 문자열 파싱
+        String[] items = orderedMenu.split(",");
+        for (String item : items) {
+            String[] parts = item.split("-");
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요");
+            }
+
+            String itemName = parts[0].trim();
+
+            Menu menu = Menu.fromString(itemName);
+
+            int quantity;
+            try {
+                quantity = Integer.parseInt(parts[1].trim());
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            if (quantity <= 0) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            order.addItem(menu, quantity);
+        }
+        return order;
+    }
 }
 
