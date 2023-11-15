563aa5f1e6e26ef004e9603913f16882ab58891b
Sim-GyuBin <skb0606@gmail.com>
2023-11-15 18:48:30 +0900

    feat: 예상 방문 날짜 입력

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -4,20 +4,29 @@ import christmas.domain.*;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.time.LocalDate;
+
 public class EventController {
     private final InputView inputView;
     private final OutputView outputView;
     private final DiscountCalculator discountCalculator;
-
+    private final BadgeAwarder badgeAwarder;
 
     public EventController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.discountCalculator = new DiscountCalculator();
+        this.badgeAwarder = new BadgeAwarder();
     }
 
     public void run() {
-
+        outputView.printGreeting();
+        try {
+            LocalDate date = inputView.readDate();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            run(); // 재귀 호출로 에러 발생 시 다시 시작
+        }
     }
 
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,19 @@
 package christmas.view;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.time.LocalDate;
 
 public class InputView {
+    public LocalDate readDate() {
+        try {
+            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            int date = Integer.parseInt(Console.readLine());
+
+            return LocalDate.of(2023,12, date);
+        } catch (IllegalArgumentException e) {
+            return readDate();
+        }
+    }
 
 }
